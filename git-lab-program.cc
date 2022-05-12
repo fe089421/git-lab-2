b47349f834483f617289d2eddf526bcaa669d9cf
fred entsie <fe089421@ohio.edu>
2022-05-12 09:17:59 -0400

    here is the C++ program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Fred Botchway Entsie>
+ *        Date: <05/12/2022>
+ * Description: Creating a c++ program in git-lab2 repository and update the online main branch.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
